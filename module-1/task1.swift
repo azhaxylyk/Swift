21f1e58a97bdb5d6b82269df6bf8945b1b86e751
azhaxylyk <zhaxylykova.a7@gmail.com>
2024-03-20 21:56:06 +0500

    new

--- module-1/task1.swift
+++ module-1/task1.swift
@@ -1,6 +1,6 @@
-let initialValue = 100  // Объявляем константу и присваиваем ей значение 100.
-var copiedValue = initialValue  // Объявляем переменную и присваиваем ей значение константы.
-let sumValue = copiedValue + initialValue  // Объявляем константу и присваиваем ей сумму значений.
+let initialValue = 100  // Объявляем константу
+var copiedValue = initialValue  // Объявляем переменную
+let sumValue = copiedValue + initialValue  // Объявляем константу и присваиваем ей сумму значений
 
 print(initialValue)
 print(copiedValue)
